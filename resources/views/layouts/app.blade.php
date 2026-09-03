8a6f98e1e12228a2b4502c05cc068481af800591
Nazwarrrr <nazwarmakarim818@gmail.com>
2026-09-03 10:39:54 +0700

    update layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Sora:wght@600;700;800&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 @stack('styles')
+<script src="https://tailwindcss.com"></script>
+
 </head>
 <body>
 <div class="dot-grid"></div>
