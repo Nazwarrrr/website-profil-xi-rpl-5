b7e5e9c60241a15ec0bc6329693d32faf9645f1c
Nazwarrrr <nazwarmakarim818@gmail.com>
2026-09-03 09:34:42 +0700

    fix vercel read-only storage and log path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,15 +4,22 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// Ubah direktori storage ke /tmp khusus di Vercel
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+    $app->useStoragePath('/tmp');
+}
+
+return $app;
